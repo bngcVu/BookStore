8e06542919fb20ce81eb96e05478f0be1988cfc0
VuCaCa <buingocvuuu@gmail.com>
2026-01-16 16:52:57 +0700

    refactor: remove unused authentication and user management classes

--- BookStore/src/main/java/com/bookstore/api/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.api.mapper;
-
-import com.bookstore.api.entity.User;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface UserMapper {
-    // Add methods as needed: toDto, fromDto
-}
